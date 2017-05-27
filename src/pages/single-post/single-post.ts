5eb17d99ed52650ac2a50a2926157d7fdc2cff88
Pierre Le Vaillant <pierrelv@live.fr>
2017-05-27 14:59:32 +0200

    ADD singlePostPage > pullToRefresh

--- src/pages/single-post/single-post.ts
+++ src/pages/single-post/single-post.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SocialSharing } from '@ionic-native/social-sharing';
+import { USMService } from '../../services/USMService';
 
 @IonicPage()
 @Component({
@@ -13,7 +14,8 @@ export class SinglePostPage {
   	constructor(
   		private navCtrl: NavController,
   		private navParams: NavParams,
-  		private socialSharing: SocialSharing
+  		private socialSharing: SocialSharing,
+      private USMService: USMService
   	) {}
 
 	ionViewDidLoad() {
@@ -24,5 +26,23 @@ export class SinglePostPage {
 		let image = this.post.featured_media ? this.post.featured_media.sizes.large.source_url : null;
 		this.socialSharing.share(this.post.title.rendered, null, image, this.post.link); 
 	}
+
+  loadPost() {
+    return new Promise(resolve => {
+      this.USMService.getPost(this.post.id).subscribe(
+                data => {
+                    resolve(data);
+                },
+                err => {}
+            );
+      });
+    }
+
+    doRefresh(refresher) {
+      this.loadPost().then( data => {
+        this.post = data;
+        refresher.complete();
+      });
+    }
 
 }
