17216006875fb871597696b5ba3173da926d58e7
Pierre-Olivier POTIER <pierrolivier@free.fr>
2017-05-27 10:25:56 +0200

    ADD: Notifications updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { OneSignal } from '@ionic-native/onesignal';
 
+import { USMService } from '../services/USMService';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
@@ -12,7 +14,13 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
     rootPage:any = TabsPage;
 
-    constructor(private platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private oneSignal: OneSignal) {
+    constructor(
+        private platform: Platform,
+        private statusBar: StatusBar,
+        private splashScreen: SplashScreen,
+        private oneSignal: OneSignal,
+        private USMService: USMService
+    ) {
       	platform.ready().then(() => {
         	// Okay, so the platform is ready and our plugins are available.
         	// Here you can do any higher level native things you might need.
@@ -34,8 +42,17 @@ export class MyApp {
 			// do something when notification is received
 		});
 
-		this.oneSignal.handleNotificationOpened().subscribe(() => {
+		this.oneSignal.handleNotificationOpened().subscribe( data => {
 		  	// do something when a notification is opened
+            let option = data.notification.payload.additionalData.data;
+            if (option.post) {
+                this.USMService.getPost(option.post).subscribe(
+                    data => {
+                        alert(data.title.rendered);
+                    },
+                    err => {}
+                );
+            }
 		});
 		this.oneSignal.endInit();
     }
